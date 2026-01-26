e7496bba7f0946840ca8395e0de5dcb6831966b9
Joren-vanGoethem <55790052+Joren-vanGoethem@users.noreply.github.com>
2026-01-26 16:46:49 +0100

    Add setup script for configuring symlinks in ~/.config/
    This script removes existing files in ~/.config/ and replaces them with symlinks to the corresponding files in .config/. It also handles specific files in the home directory.

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#############################################################################
+# !!!!!!!!!!!!!!!!!!!!!!!!!!! READ BEFORE USING !!!!!!!!!!!!!!!!!!!!!!!!!!! #
+#																	                                      		#
+# THIS REMOVES ALL FILES IN ~/.config/ THAT MATCH WITH THE FILES IN .config #
+# AND REPLACES THEM WITH SYMLINKS TO THE FILES IN .config/ 				        	#
+# 																		                                    	#
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! #
+#############################################################################
+
+
+echo -e "\nWARNING!! This will remove all files in ~/.config/ that match with the files in .config/ and replace them with symlinks to the files in .config/. Make sure you have backups!\n"
+read -rep "Are you sure you want to continue? [y/N] "
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+	echo "Aborting..."
+	exit 1
+fi
+
+echo -e "Setting up config files..."
+
+# for every file in .config/,
+# remove the file in ~/.config/
+# and link it to the file in .config/
+for file in .config/*; do
+	rm -rf ~/.config/$(basename $file)
+	echo "Linking $file to ~/.config/$(basename $file)"
+	ln -s $(pwd)/$file ~/.config/$(basename $file)
+done
+
+# do the same for specific files in .
+for file in .Xresources; do
+	rm -rf ~/$file
+	echo "Linking $file to ~/$file"
+	ln -sf $(pwd)/$file ~/$file
+done
+
+echo -e "\nDone!"
